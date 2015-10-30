7443fd16d1c72623cfaae0d35a820e231870dcac
Will Faurot <will@influxdb.com>
2015-10-29 19:21:11 -0700

    Add redux boilerplate and directory structure

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import 'babel-core/polyfill';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Root from './pages/Root';
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root')
+);
---
+++ app/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  placeholder
+});
+
+function placeholder(state = {}) {
+  return state;
+}
+
+export default rootReducer;
